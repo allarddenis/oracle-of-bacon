3953a8f2d4d308fe3657cfbca806c755750d8b6e
alexisrobin <alexisrobin.bonjour@gmail.com>
2018-02-21 10:41:19 +0100

    Loading data in elasticsearch through CompletionLoader

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/loader/elasticsearch/CompletionLoader.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/loader/elasticsearch/CompletionLoader.java
@@ -1,13 +1,22 @@
 package com.serli.oracle.of.bacon.loader.elasticsearch;
 
 import com.serli.oracle.of.bacon.repository.ElasticSearchRepository;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RestHighLevelClient;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
 public class CompletionLoader {
     private static AtomicInteger count = new AtomicInteger(0);
@@ -22,13 +31,31 @@ public class CompletionLoader {
         }
 
         String inputFilePath = args[0];
+
+        Stack<BulkRequest> brStack = new Stack<>();
+        brStack.push(new BulkRequest());
         try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(inputFilePath))) {
             bufferedReader
                     .lines()
-                    .forEach(line -> {
-                        //TODO ElasticSearch insert
-                        System.out.println(line);
-                    });
+                        .forEach((String line) -> {
+
+                            // Packaging IndexRequest by 5MB's BulkRequest
+                            if(brStack.peek().estimatedSizeInBytes() > 5242880) {
+                                brStack.push(new BulkRequest());
+                            }
+
+                            Map<String, Object> jsonMap = new HashMap<>();
+                            jsonMap.put("fullname", line);
+                            brStack.peek().add(
+                             new IndexRequest("imdb", "actor").source(jsonMap)
+                            );
+
+            });
+        }
+
+        while(!brStack.empty()) {
+            count.addAndGet(client.bulk(brStack.pop()).getItems().length);
+            System.out.println("... inserted actors: " + count);
         }
 
         System.out.println("Inserted total of " + count.get() + " actors");
